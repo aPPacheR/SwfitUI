91dbdb30e127c09f8ec4edcf0604df1dc9f7bcab
Pavlenko Pavel <87997793+aPPacheR@users.noreply.github.com>
2025-11-18 19:46:46 +0300

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Павленко Павел on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
