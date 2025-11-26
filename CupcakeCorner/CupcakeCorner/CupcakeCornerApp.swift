c4e34c6a77c5a985597cb82e3806492fd3b823b5
Pavlenko Pavel <pavlnkopavel@gmail.com>
2025-11-26 17:30:16 +0300

    Add CupcakeCorner

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Павленко Павел on 26.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
