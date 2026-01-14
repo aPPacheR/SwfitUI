49d0e6dce918c238fda53773562ea06f5f525ac5
Pavlenko Pavel <pavlnkopavel@gmail.com>
2026-01-14 15:40:12 +0300

    Add InstaFilter

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Павленко Павел on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
